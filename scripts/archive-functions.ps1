b83697679f6d7054890688e0baff5fed84201c9a
Baptiste BOQUAIN <baptisteboquain@isipharm.fr>
2025-05-27 15:40:36 +0200

    ✨ Update edge functions deployment to extract and start functions from tar archive

---
+++ scripts/archive-functions.ps1
@@ -0,0 +1,11 @@
+# Suppression du fichier existant s'il existe
+$targetPath = "../k8s/supabase/base/edge-functions/functions.tar"
+if (Test-Path $targetPath) {
+    Remove-Item $targetPath -Force
+}
+
+# Création de l'archive TAR
+tar -cvf functions.tar -C ../k8s/supabase/base/edge-functions .
+
+# Déplacement du nouveau fichier
+mv functions.tar $targetPath
