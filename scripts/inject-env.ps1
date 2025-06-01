db02de9cc96fccd451badf06574e6301985784f8
Baptiste BOQUAIN <baptisteboquain@isipharm.fr>
2025-06-01 20:54:11 +0200

    🔧 Change Traefik service type from NodePort to LoadBalancer

--- scripts/inject-env.ps1
+++ scripts/inject-env.ps1
@@ -1,3 +1,7 @@
 kubectl create namespace supabase
 kubectl create secret generic common-secrets --from-env-file=../k8s/supabase/base/.env.secrets --namespace supabase
 kubectl create configmap common-config --from-env-file=../k8s/supabase/base/.env --namespace supabase
+
+#deploy traefik crds
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml --namespace supabase
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml --namespace supabase
