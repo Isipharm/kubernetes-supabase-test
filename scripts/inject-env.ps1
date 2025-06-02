7b974953f0f40aa82e183a70e0baf6ba30bd2d41
Baptiste BOQUAIN <baptisteboquain@isipharm.fr>
2025-06-02 20:32:56 +0200

    🔧 Update kong configuration and enable gateway APIs

--- scripts/inject-env.ps1
+++ scripts/inject-env.ps1
@@ -4,5 +4,6 @@ kubectl create secret generic basicauth-secrets --from-file=users=../k8s/supabas
 kubectl create configmap common-config --from-env-file=../k8s/supabase/base/.env --namespace supabase
 
 #deploy traefik crds
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml --namespace supabase
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml --namespace supabase
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml
+#kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml --namespace supabase
+#kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml --namespace supabase
