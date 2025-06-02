26314794ba14e07e535b093561eee8b3f76c8f42
Baptiste BOQUAIN <baptisteboquain@isipharm.fr>
2025-06-02 21:57:41 +0200

    🔐 Add environment files for dashboard authentication and update secret creation script

--- scripts/inject-env.ps1
+++ scripts/inject-env.ps1
@@ -1,10 +1,13 @@
 kubectl create namespace supabase
 kubectl create secret generic common-secrets --from-env-file=../k8s/supabase/base/.env.secrets --namespace supabase
-kubectl create secret generic basicauth-secrets --from-file=users=../k8s/supabase/base/users.txt --namespace supabase
+#kubectl create secret generic basicauth-secrets --from-file=users=../k8s/supabase/base/users.txt --namespace supabase
+kubectl create secret generic service-key-auth --from-env-file=../k8s/supabase/base/servicekey.env --namespace supabase
+kubectl create secret generic anon-key-auth --from-env-file=../k8s/supabase/base/anonkey.env --namespace supabase
+kubectl create secret generic dashboard-auth --from-env-file=../k8s/supabase/base/dashboard-auth.env --namespace supabase
 kubectl create configmap common-config --from-env-file=../k8s/supabase/base/.env --namespace supabase
 
 #deploy traefik crds
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml --namespace supabase
 
 #kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml --namespace supabase
 #kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml --namespace supabase
