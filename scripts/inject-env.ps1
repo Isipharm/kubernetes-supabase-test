1ee03b1cffa3492eee51f1b29b4560d91ad1e9e4
Baptiste BOQUAIN <baptisteboquain@isipharm.fr>
2025-06-03 12:14:53 +0200

    🔧 Refactor key-auth plugin configuration and update routes to use key-auth

--- scripts/inject-env.ps1
+++ scripts/inject-env.ps1
@@ -1,9 +1,9 @@
 kubectl create namespace supabase
 kubectl create secret generic common-secrets --from-env-file=../k8s/supabase/base/.env.secrets --namespace supabase
 #kubectl create secret generic basicauth-secrets --from-file=users=../k8s/supabase/base/users.txt --namespace supabase
-kubectl create secret generic service-key-auth --from-env-file=../k8s/supabase/base/servicekey.env --namespace supabase
+#kubectl create secret generic service-key-auth --from-env-file=../k8s/supabase/base/servicekey.env --namespace supabase
 #kubectl label secret service-key-auth konghq.com/credential=key-auth --namespace supabase
-kubectl create secret generic anon-key-auth --from-env-file=../k8s/supabase/base/anonkey.env --namespace supabase
+#kubectl create secret generic anon-key-auth --from-env-file=../k8s/supabase/base/anonkey.env --namespace supabase
 #kubectl label secret anon-key-auth konghq.com/credential=key-auth --namespace supabase
 
 
