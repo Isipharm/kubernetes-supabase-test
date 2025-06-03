0b0c9c248d7a0d664ee8f6474df955ec599b1871
Baptiste BOQUAIN <baptisteboquain@isipharm.fr>
2025-06-03 10:16:16 +0200

    🔧 Rename dashboard-basic-auth to basic-auth in kong-plugins.yaml and update references in kong-routes.yaml

--- scripts/inject-env.ps1
+++ scripts/inject-env.ps1
@@ -2,16 +2,17 @@ kubectl create namespace supabase
 kubectl create secret generic common-secrets --from-env-file=../k8s/supabase/base/.env.secrets --namespace supabase
 #kubectl create secret generic basicauth-secrets --from-file=users=../k8s/supabase/base/users.txt --namespace supabase
 kubectl create secret generic service-key-auth --from-env-file=../k8s/supabase/base/servicekey.env --namespace supabase
-kubectl label secret service-key-auth konghq.com/credential=key-auth --namespace supabase
+#kubectl label secret service-key-auth konghq.com/credential=key-auth --namespace supabase
 kubectl create secret generic anon-key-auth --from-env-file=../k8s/supabase/base/anonkey.env --namespace supabase
-kubectl label secret anon-key-auth konghq.com/credential=key-auth --namespace supabase
-kubectl create secret generic dashboard-basic-auth --from-env-file=../k8s/supabase/base/dashboard-auth.env --namespace supabase
-kubectl label secret dashboard-basic-auth konghq.com/credential=basic-auth --namespace supabase
+#kubectl label secret anon-key-auth konghq.com/credential=key-auth --namespace supabase
+kubectl create secret generic basic-auth --from-env-file=../k8s/supabase/base/dashboard-auth.env --namespace supabase
+#kubectl label secret basic-auth konghq.com/credential=basic-auth --namespace supabase
 kubectl create configmap common-config --from-env-file=../k8s/supabase/base/.env --namespace supabase
 
 #deploy traefik crds
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml --namespace supabase
-kubectl apply -f ../k8s/supabase/base/kong-gateway.yaml
+kubectl apply -f ../k8s/supabase/base/kong-gateway.yaml --namespace supabase
+kubectl apply -f ../k8s/supabase/base/kong-plugins.yaml --namespace supabase
 helm repo add kong https://charts.konghq.com
 helm repo update
 helm install kong kong/ingress -n supabase
